081f33c0f906e8254b94c8008867f2c0820059e8
Scott Brown <scottdbrown7@gmail.com>
2023-11-18 17:13:10 -0700

    Move spotifyapi to dedicated repo

--- spotifyapi/README.md
+++
@@ -1,1 +0,0 @@
-`spotifyapi` is a package to streamline the process of querying and cleaning data from the Spotify Web API.
--- spotifyapi/spotifyapi/api_constants.py
+++
@@ -1,3 +0,0 @@
-DATA_DIR = './data'
-ARTISTS_ENDPOINT = 'https://api.spotify.com/v1/artists'
-AUDIO_FEATURES_ENDPOINT = 'https://api.spotify.com/v1/audio-features'
