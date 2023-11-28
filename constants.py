8f254eec8c26cb76d9f0d355dea3e4f23dae61e3
Scott Brown <scottdbrown7@gmail.com>
2023-11-27 18:39:16 -0700

    Start LightGBM multiclassification

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
+## Data
 DATA_DIR = './data'
 ARTISTS_ENDPOINT = 'https://api.spotify.com/v1/artists'
 AUDIO_FEATURES_ENDPOINT = 'https://api.spotify.com/v1/audio-features'
+
+## Modeling
+# NOTE: May want to convert release date to time series features (e.g. year, month, day, etc.)
+DROP_COLS = ['Unnamed: 0','artist_id','name','track_id','track_name','release_date','release_date_precision','Map']
