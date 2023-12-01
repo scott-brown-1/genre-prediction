d394196dce02ea22c45ab546c6ecbb615e26dd95
Scott Brown <scottdbrown7@gmail.com>
2023-11-30 20:42:15 -0700

    Finialize multilabel accuracy eval and add SHAP

--- modeling/load_train_test.py
+++ modeling/load_train_test.py
@@ -12,7 +12,7 @@ from constants import DATA_DIR, DROP_COLS
 
 TARGET = 'Genre'
 
-def load_train_test(multilabel=False, scale=False):
+def load_train_test(scale=False):
     data = pd.read_csv(f'{DATA_DIR}/Final_Data_For_Real.csv')
 
     ## Prepare data for modeling
@@ -38,8 +38,7 @@ def load_train_test(multilabel=False, scale=False):
         X_test = sc.transform(X_test)
 
     y_train = y_train.str.findall(r"'(.*?)'").apply(lambda x: x[0])
-    if(multilabel):
-        y_test = y_test.str.findall(r"'(.*?)'")
+    y_test = y_test.str.findall(r"'(.*?)'")
         
     return X_train, X_test, y_train, y_test
 
--- modeling/predict_and_evaluate.py
+++ modeling/predict_and_evaluate.py
@@ -2,16 +2,31 @@ import warnings
 import pandas as pd
 from sklearn.metrics import accuracy_score
 
-def predict_and_evaluate(model, new_data, truth_data, multilabel=False):
-    if(multilabel):
-        #warnings.warn('Multi-label classification not yet supported. Returning single-label data.')
-        #y_pred = predict_and_evaluate(model, new_data, truth_data, multilabel=False)
-        #return y_pred
-        y_pred = model.predict_proba(new_data)
-    else:
-        truth_data = truth_data.str.findall(r"'(.*?)'").apply(lambda x: x[0])
-        y_pred = model.predict(new_data)
-        accuracy = accuracy_score(truth_data, y_pred)
+def predict_and_evaluate(model, new_data, truth_data):
+    ## Predict new data
+    y_pred = pd.DataFrame(model.predict(new_data))
+    y_prob = pd.DataFrame(model.predict_proba(new_data), columns=model.classes_)
 
-    #print(accuracy)
-    return pd.DataFrame(y_pred, columns=model.classes_)
+    ## Calculate accuracy of predicting top genre
+    top_genre_accuracy = accuracy_score(truth_data.apply(lambda x: x[0]), y_pred)
+    print(f'Top genre accuracy: {top_genre_accuracy}')
+
+    ## Calculate accuracy of predicting all genres
+    total_correct = 0
+    total_genres = 0
+
+    for i,genres in enumerate(truth_data):
+        top_k_preds = y_prob.loc[i,:].nlargest(len(genres)).index.tolist()
+
+        # Count the number of correct predictions
+        correct_preds = len(set(top_k_preds).intersection(set(genres)))
+        total_correct += correct_preds
+        total_genres += len(genres)
+        
+        #accuracy = correct_preds / len(genres)
+        #print(f'Accuracy for {i}: {accuracy}')
+    
+    all_genre_accuracy = total_correct / total_genres
+    print(f'All genre accuracy: {all_genre_accuracy}')
+
+    return y_pred, y_prob
