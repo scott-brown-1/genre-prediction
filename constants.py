f45a3c26aaf337869bd1942458fcd4632214a5d0
Scott Brown <scottdbrown7@gmail.com>
2023-12-04 21:08:53 -0700

    Fix train test split to split by artist id rather than random

--- constants.py
+++ constants.py
@@ -5,4 +5,4 @@ AUDIO_FEATURES_ENDPOINT = 'https://api.spotify.com/v1/audio-features'
 
 ## Modeling
 # NOTE: May want to convert release date to time series features (e.g. year, month, day, etc.)
-DROP_COLS = ['Unnamed: 0','artist_id','name','track_id','track_name','release_date','release_date_precision','Map','genres']
+DROP_COLS = ['Unnamed: 0','name','track_id','track_name','release_date','release_date_precision','Map','genres']
--- modeling/load_train_test.py
+++ modeling/load_train_test.py
@@ -4,6 +4,7 @@
 
 import warnings
 import pandas as pd
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -13,28 +14,64 @@ from constants import DATA_DIR, DROP_COLS
 
 TARGET = 'Genre'
 
+## Create train test split breaking on artist_id
+def artist_train_test_split(df, test_size = 0.3):
+    ## Get unique artist ids and randomize order
+    artist_ids = df['artist_id'].unique()
+    np.random.shuffle(artist_ids)
+
+    ## Get indices of test and train
+    split = int(len(artist_ids) * test_size)
+    test_artist_ids = artist_ids[:split]
+    train_artist_ids = artist_ids[split:]
+
+    ## Get train and test df
+    train = df[df['artist_id'].isin(train_artist_ids)]
+    test = df[df['artist_id'].isin(test_artist_ids)]
+
+    ## Drop artist_id from train and test
+    train = train.drop('artist_id', axis=1)
+    test = test.drop('artist_id', axis=1)
+    
+    return train, test
+
 def load_train_test(scale=False, upsample=True, encode=False):
     data = pd.read_csv(f'{DATA_DIR}/Final_Data_For_Real.csv')
 
     ## Prepare data for modeling
     data = data.drop(DROP_COLS, axis=1)
 
+    ## Shuffle data
+    data = data.sample(frac=1, random_state=42)
+
     ## Convert string columns to categorical
     # TODO: Target encode! This defaults to one-hot
     for col in data.columns:
-        if data[col].dtype == 'object' and col != TARGET:
+        if (data[col].dtype == 'object' and 
+            col != TARGET and
+            col != 'artist_id'):
             data[col] = data[col].astype('category')
 
     data['time_signature'] = data['time_signature'].astype('category')
 
-    X = data.drop(TARGET,axis=1)
-    y = data[TARGET]
+    train, test = artist_train_test_split(data, test_size=0.3)
+
+    ## Split train and test into X_train, X_test, y_train, y_test
+    X_train = train.drop(TARGET,axis=1)
+    y_train = train[TARGET]
+
+    X_test = test.drop(TARGET,axis=1)
+    y_test = test[TARGET]
+
+    #X = data.drop(TARGET,axis=1)
+    #y = data[TARGET]
 
     if(upsample):
+        print('Upsampling data not yet implemented')
         sm = SMOTE(random_state=42)
-        X, y = sm.fit_resample(X, y)
+        X_train, y_train = sm.fit_resample(X_train, y_train)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 
     ## Manual one-hot encoding
     if(encode):
